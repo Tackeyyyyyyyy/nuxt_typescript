8e3c7556272166fff705c3a30043468d12e6a3b6
Tackeyyyyyyyy <ftakizawa@schoo.jp>
2021-08-08 18:16:08 +0900

    up

---
+++ components/TestComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>Name: {{ profileName }} </div>
+    <div>Sex: {{ profileSex }} </div>
+    <div>Age: {{ profileAge }}  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop} from 'vue-property-decorator'
+
+interface Profile {
+  name: string
+  sex: bigint
+  age: bigint
+}
+
+@Component
+export default class TestComponent extends Vue {
+  @Prop({type: Object, required: true}) readonly profile!: Profile
+
+  message: string = 'This is a message'
+
+  get profileName(): string {
+    return `${this.profile.name} `
+  }
+
+  get profileAge(): bigint {
+    return this.profile.age
+  }
+
+  get profileSex(): string {
+    if (this.profile.sex) {
+      return "men"
+    }
+    return "women"
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>This is profile</h1>
+    <TestComponent :profile="testProfile"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import TestComponent from "~/components/TestComponent.vue";
+
+@Component({
+  components: {
+    TestComponent,
+  },
+})
+
+export default class IndexPage extends Vue {
+  testProfileInfo = {
+    name: "this is a title",
+    sex: 1,
+    age: 30,
+  }
+
+  get testProfile() {
+    return this.testProfileInfo
+  }
+}
+</script>
